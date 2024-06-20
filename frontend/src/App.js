d16badc01be3039263ab5fc48e01dd7b50347189
mitchspiron <mitchspiron280@gmail.com>
2024-06-21 02:24:11 +0300

    refact: make all routes in a single file out of App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,6 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Single from "./pages/Single";
-import Write from "./pages/Write";
-import Layout from "./components/Layout";
-import "./style.scss"
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/post/:id",
-        element: <Single />,
-      },
-      {
-        path: "/write",
-        element: <Write />,
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+import "./style.scss";
 
 function App() {
   return (
---
+++ frontend/src/routes.js
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+import Layout from "./components/Layout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
